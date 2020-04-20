4c30f6ba97031685d31243878f33b621d6813d8d
OldPig <446120447@qq.com>
2020-04-20 17:31:06 +0800

    Update autoWh_Python.py
    开头填写操作信息，方便使用

--- autoWh_Python.py
+++ autoWh_Python.py
@@ -1,16 +1,21 @@
 from openpyxl import load_workbook
-from openpyxl import Workbook
-import openpyxl
-import numpy
 import pandas as pd
+import os
+
+
+#需要填写的参数：
+readFileName = 'cn_5934_muestras_todos_los_meses.csv'   #要读取的csv文档名
+writeFileName = 'Comparativa_Susana.xlsx'               #需要录入的表格名
+resultFileName = 'Comparativa_Susana-test.xlsx'         #保存结果的文档名
+
 
 #加载参数文件
-data1 = pd.read_csv('cn_5934_muestras_todos_los_meses.csv', encoding='utf-8')
+data1 = pd.read_csv(readFileName, encoding='utf-8')
 data1["Production"] = data1["Production"] * 1000
-data1.to_excel('cn_5934_muestras_todos_los_meses.xlsx', sheet_name='Production')
+data1.to_excel('somethingYouNeed.xlsx', sheet_name='Production')
 
-wb1 = load_workbook('cn_5934_muestras_todos_los_meses.xlsx')
-wb2 = load_workbook('Comparativa_Susana.xlsx')
+wb1 = load_workbook('somethingYouNeed.xlsx')
+wb2 = load_workbook(writeFileName)
 
 
 #读取workbook中所有表格
@@ -24,8 +29,8 @@ print(sheets2)
 sheet1 = wb1.get_sheet_by_name(sheets1[0])
 sheet2 = wb2.get_sheet_by_name(sheets2[0])
 
-max_row = sheet2.max_row                    #最大行数
-max_column = sheet2.max_column              #最大列数
+max_row = sheet2.max_row                #最大行数
+max_column = sheet2.max_column          #最大列数
 
 #填写4-1到12-31数据
 for m in range(2, max_row+1-2160):
@@ -33,7 +38,7 @@ for m in range(2, max_row+1-2160):
     o = 108
     n = chr(n)                          #ASCII字符
     o = chr(o)
-    i = '%s%d' % (o, m+2160)                 #读取单元格编号
+    i = '%s%d' % (o, m+2160)            #读取单元格编号
     j = '%s%d' % (n, m)                 #写入单元格编号
     cell1 = sheet1[i].value             #获取data单元格数据
     sheet2[j].value = cell1             #赋值到test单元格
@@ -44,7 +49,7 @@ for m in range(6602, 8018):
     o = 108
     n = chr(n)                          #ASCII字符
     o = chr(o)
-    i = '%s%d' % (o, m-6600)                 #读取单元格编号
+    i = '%s%d' % (o, m-6600)            #读取单元格编号
     j = '%s%d' % (n, m)                 #写入单元格编号
     cell1 = sheet1[i].value             #获取data单元格数据
     sheet2[j].value = cell1             #赋值到test单元格
@@ -71,7 +76,8 @@ for m in range(8042, max_row+1):
     cell1 = sheet1[i].value             #获取data单元格数据
     sheet2[j].value = cell1             #赋值到test单元格
 
-wb2.save('Comparativa_Susana-test.xlsx')    #保存数据
+wb2.save(resultFileName)                #保存数据
 print("写入完成")
-wb1.close()                                 #关闭excel
+os.remove('somethingYouNeed.xlsx')
+wb1.close()                             #关闭excel
 wb2.close()
