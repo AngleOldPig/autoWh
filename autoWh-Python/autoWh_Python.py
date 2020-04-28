e9e96eb662ddc190b5d5d0e1c448e4c56851c93d
OldPig <446120447@qq.com>
2020-04-28 17:23:44 +0800

    增加实际值读取模式4

--- autoWh-Python/autoWh_Python.py
+++ autoWh-Python/autoWh_Python.py
@@ -12,23 +12,24 @@ np.set_printoptions(threshold=np.inf)
 # *** 注意 *** 不要填写后缀名，也不要填写文件名的日期部分
 predictFileName = '5934_muestras_todos_los_meses'  # 要读取的预测数据文档名
 predictReadColumn = 'k'     # 预测数据读取列名(仅支持小写)
-predictFileReadMode = 1     # 填1或者2，玄学选项，若导出乱码则尝试另一个模式
-actualFileName = 'semana'  # 要读取的实际数据文档名
+predictFileReadMode = 2     # 填1：短参数 或者 2：长参数，玄学选项，若导出乱码则尝试另一个模式
+actualFileName = 'download'  # 要读取的实际数据文档名
 actualFileMode = 3  # 实际数据读取模式
-actualReadColumn = 2  # 实际数据读取的列名，模式3选择整数部分的列名
+actualReadColumn = 2  # 实际数据读取的列名，模式4选择整数部分的列名
 writeFileName = 'Comparativa_Susana'  # 需要录入的表格名
 resultFileName = 'Comparativa_Susana-finish'  # 保存结果的文档名# *** 注意 *** 不要填写后缀名，也不要填写文件名的日期部分
 # *** 注意 *** 不要填写后缀名，也不要填写文件名的日期部分
 # 实际数据读取模式：
 # 模式1. 时间命名用yyyy_mm_dd表示，每日一个文件
 # 模式2. 时间命名用yyyy-mm-dd表示，每日一个文件，数据大小自动×1000
-# 模式3. 文件命名用1、2、3。。。表示，每周一个文件
+# 模式3. 文件命名用(1)、(2)、(3)。。。表示，每周一个文件
+# 模式4. 文件命名用1、2、3。。。表示，每周一个文件
 
 
 # 加载参数文件：
 print("读取中。。。")
 
-# 使用pd.read_csv需手动在36行更改读取参数，注意保存左侧对齐
+# 使用pd.read_csv有时需手动更改读取参数，注意保存左侧对齐
 # 国内（delimiter=";", decimal=",", thousands='.',)
 # 西班牙（delimiter=";",）
 
@@ -225,9 +226,89 @@ if actualFileMode == 2:
                 # 将时间信息与每小时平均数一并存入csv文件：
                 data2TempDataFrame.to_csv(actualProductionCsvName, mode='a', header=False, index=None)
 
-# 模式3. 文件命名用1、2、3。。。表示
+# 模式3. 文件命名用(1)、(2)、(3)。。。表示，每周一个文件
 if actualFileMode == 3:
-    for n in range(0, 53):
+    for n in range(0, 54):
+        # 组合生成文件名
+        if n == 0:
+            data2FileName = actualFileName + '.csv'
+        else:
+            data2FileName = actualFileName + ' (' + str(n) + ')' + '.csv'
+        # 判断当前名称的文件是否存在
+        if os.path.exists(data2FileName):
+            if os.path.getsize(data2FileName):
+                print('已读取' + data2FileName)
+            else:
+                print(data2FileName + '文件存在但为空')
+                continue
+        else:
+            # print(data2FileName + '文件不存在')
+            continue
+
+        # 用暂存变量dataTemp读取生成文档名指向的csv文件
+        # 备用参数 , usecols=[0, actualReadCsvColumn, actualReadColumn]
+        data2Temp = pd.read_csv(data2FileName, encoding='utf-8', header=None)
+        # 添加空白列以防止读取报错
+        if data2Temp.shape[1] < 3:
+            data2Temp[2] = 0
+        # 用0替换DataFrame对象中所有的空值
+        data2Temp = data2Temp.fillna(0)
+        # 去除数据中的--并换成0
+        data2Temp = data2Temp.replace('--', 0)
+        # 准备参数
+        data2TempList = []
+        a = ''
+        y = ''
+        m = ''
+        d = ''
+        h = ''
+        p = 0.0
+        q = 0.0
+        num = [0.0, 0.0, 0.0]
+
+        # 调试用的信息
+        # print('表格模板：')
+        # print(dataTempList)
+        # print('选取项：')
+        # print(dataTemp[0][3])
+        # print('所有项：')
+        # print(data2Temp)
+
+        # 将文件中的实际值读取出并存入num数组
+        for i in range(5, 173):
+            if i == 5:
+                continue
+            if (i - 5) % 24 == 0:
+                data2TempList.append([y, m, d, 24, 0])
+                continue
+            p = data2Temp[actualReadCsvColumn][i]
+            q = data2Temp[actualReadColumn][i]
+            # 读取时间
+            a = data2Temp[0][i]
+            y = a[0:4]
+            m = a[5:7]
+            d = a[8:10]
+            h = a[11:13]
+            h = h.replace(':', '')
+            # 读取数据
+            num[0] = float(p)
+            num[1] = float(q)
+            num[2] = num[0] + (num[1]/100)
+            data2TempList.append([y, m, d, h, num[2]])  # 将时间数据和每小时平均数添加进dataTempList列表
+        data2TempList.append([y, m, d, 24, 0])
+
+        # 将 列表list 转换为 DataFrame格式
+        data2TempDataFrame = pd.DataFrame(data2TempList)
+        # print('表格雏形：')
+        # print(data2TempDataFrame)
+
+        # 将时间信息与每小时平均数一并存入csv文件：
+        data2TempDataFrame.to_csv(actualProductionCsvName, mode='a', header=False, index=None)
+
+
+# 模式4. 文件命名用1、2、3。。。表示
+if actualFileMode == 4:
+    for n in range(0, 54):
         # 组合生成文件名
         data2FileName = actualFileName + str(n) + '.csv'
         # 判断当前名称的文件是否存在
@@ -316,7 +397,7 @@ wb1 = load_workbook('somethingYouNeed.xlsx')
 wb3 = load_workbook(writeFileName + '.xlsx')
 
 # 读取workbook中所有表格
-print('请忽略以下4条报警：')
+print('请忽略这4条报警：')
 sheets1 = wb1.get_sheet_names()
 sheets3 = wb3.get_sheet_names()
 
@@ -335,7 +416,7 @@ max_column = sheet3.max_column  # 最大列数
 # rowWriteStart：开始写入的行标号
 # rowWriteStop：停止写入的行标号
 # rowReadFixed：读取与写入行之间的差值
-def xlsxDataCopy(columnRead, columnWrite, rowWriteStart, rowWriteStop, rowReadFixed):
+def predictXlsxDataCopy(columnRead, columnWrite, rowWriteStart, rowWriteStop, rowReadFixed):
     "根据行列编号复制单元格内容"
     for m in range(rowWriteStart, rowWriteStop):
         n = columnWrite
@@ -352,13 +433,13 @@ def xlsxDataCopy(columnRead, columnWrite, rowWriteStart, rowWriteStop, rowReadFi
 print("写入中。。。")
 
 # 写入预测数据
-xlsxDataCopy(predictReadColumnAscii, 101, 2, max_row + 1 - 2160, 2160)
-xlsxDataCopy(predictReadColumnAscii, 101, 6602, 8018, -6600)
-xlsxDataCopy(predictReadColumnAscii, 101, 8018, 8042, -6624)
-xlsxDataCopy(predictReadColumnAscii, 101, 8042, max_row + 1, -6624)
-
+predictXlsxDataCopy(predictReadColumnAscii, 101, 2, max_row + 1 - 2160, 2160)
+predictXlsxDataCopy(predictReadColumnAscii, 101, 6602, 8018, -6600)
+predictXlsxDataCopy(predictReadColumnAscii, 101, 8018, 8042, -6624)
+predictXlsxDataCopy(predictReadColumnAscii, 101, 8042, max_row + 1, -6624)
+print("已写入预测数据")
 # 写入实际数据
-
+# 需学习dict的使用
 
 # 完成收尾工作
 wb3.save(resultFileName + '.xlsx')  # 保存数据
